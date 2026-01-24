14811778ec7defbc25b41a8664b01b7bbc546213
AntonDor <dorogovanton291@gmail.com>
2026-01-24 15:36:58 +0300

    feat: practice sprint_2 REST API module_3

--- src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
@@ -1,83 +1,41 @@
 package ru.yandex.practicum.catsgram.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.catsgram.exception.ConditionsNotMetException;
-import ru.yandex.practicum.catsgram.exception.DuplicatedDataException;
 import ru.yandex.practicum.catsgram.model.User;
+import ru.yandex.practicum.catsgram.service.UserService;
 
-import java.time.Instant;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-    private final Map<Long, User> users = new HashMap<>();
+    UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping
     public Collection<User> findAll() {
-        return users.values();
+        return userService.findAll();
+    }
+
+    @GetMapping("/{userId}")
+    public Optional<User> findById(@PathVariable long userId) {
+        return userService.findById(userId);
     }
 
     @PostMapping
     public User create(@RequestBody User user) {
-        if (user.getEmail() == null || user.getEmail().isBlank()) {
-            throw new ConditionsNotMetException("Имейл должен быть указан");
-        }
-        if (users
-                .values()
-                .stream()
-                .anyMatch(user1 -> user1.getEmail().equals(user.getEmail()))
-        ) {
-            throw new DuplicatedDataException("Этот имейл уже используется");
-        }
-        user.setId(getNextId());
-        user.setRegistrationDate(Instant.now());
-        users.put(user.getId(), user);
-        return user;
+        return userService.create(user);
     }
 
     @PutMapping
     public User update(@RequestBody User newUser) {
-        if (newUser.getId() == null) {
-            throw new ConditionsNotMetException("Id должен быть указан");
-        }
-        User oldUser = users.get(newUser.getId());
-        if (oldUser == null) {
-            throw new ConditionsNotMetException("Пользователь не найден");
-        }
-        String newEmail = newUser.getEmail();
-        if (newEmail != null) {
-            if (users
-                    .values()
-                    .stream()
-                    .anyMatch(user1 -> user1.getEmail().equals(newEmail))
-            ) {
-                throw new DuplicatedDataException("Этот имейл уже используется");
-            }
-        }
-        if (newEmail != null) {
-            oldUser.setEmail(newEmail);
-        }
-        String newUsername = newUser.getUsername();
-        if (newUsername != null) {
-            oldUser.setUsername(newUsername);
-        }
-        String newPassword = newUser.getPassword();
-        if (newPassword != null) {
-            oldUser.setPassword(newPassword);
-        }
-        return oldUser;
-    }
-
-    private long getNextId() {
-        long currentMaxId = users.keySet()
-                .stream()
-                .mapToLong(id -> id)
-                .max()
-                .orElse(0);
-        return ++currentMaxId;
+        return userService.update(newUser);
     }
 }
---
+++ src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
@@ -0,0 +1,70 @@
+package ru.yandex.practicum.catsgram.service;
+
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.catsgram.exception.ConditionsNotMetException;
+import ru.yandex.practicum.catsgram.exception.NotFoundException;
+import ru.yandex.practicum.catsgram.model.Post;
+
+import java.time.Instant;
+import java.util.*;
+
+@Service
+public class PostService {
+    private final Map<Long, Post> posts = new HashMap<>();
+
+    public Collection<Post> findAll(long from, long size, String sort) {
+
+        Comparator<Post> comparator = Comparator.comparing(Post::getPostDate);
+
+        if (sort != null && sort.equals("desc")) {
+            comparator = comparator.reversed();
+        }
+
+        List<Post> list = posts.values().stream()
+                .sorted(comparator)
+                .skip(from)
+                .limit(size)
+                .toList();
+
+        return list;
+    }
+
+    public Optional<Post> findById(long id) {
+        return Optional.ofNullable(posts.get(id));
+    }
+
+    public Post create(Post post) {
+        if (post.getDescription() == null || post.getDescription().isBlank()) {
+            throw new ConditionsNotMetException("Описание не может быть пустым");
+        }
+
+        post.setId(getNextId());
+        post.setPostDate(Instant.now());
+        posts.put(post.getId(), post);
+        return post;
+    }
+
+    public Post update(Post newPost) {
+        if (newPost.getId() == null) {
+            throw new ConditionsNotMetException("Id должен быть указан");
+        }
+        if (posts.containsKey(newPost.getId())) {
+            Post oldPost = posts.get(newPost.getId());
+            if (newPost.getDescription() == null || newPost.getDescription().isBlank()) {
+                throw new ConditionsNotMetException("Описание не может быть пустым");
+            }
+            oldPost.setDescription(newPost.getDescription());
+            return oldPost;
+        }
+        throw new NotFoundException("Пост с id = " + newPost.getId() + " не найден");
+    }
+
+    private long getNextId() {
+        long currentMaxId = posts.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+}
